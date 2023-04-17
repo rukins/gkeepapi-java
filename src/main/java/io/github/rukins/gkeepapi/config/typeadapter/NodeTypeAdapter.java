34f4622222526dc2ca808f9a4832bd0aa8117df9
Sergey <sergey.rukin1425@gmail.com>
2023-04-17 22:34:38 +0500

    update NodeTypeAdapter, fix bug with throwing StackOverflowError if 'type' of node was null

--- src/main/java/io/github/rukins/gkeepapi/config/typeadapter/NodeTypeAdapter.java
+++ src/main/java/io/github/rukins/gkeepapi/config/typeadapter/NodeTypeAdapter.java
@@ -6,14 +6,17 @@ import io.github.rukins.gkeepapi.model.gkeep.node.nodeobject.*;
 
 import java.lang.reflect.Type;
 
-public class NodeTypeAdapter implements JsonSerializer<Node>, JsonDeserializer<Node>  {
+public class NodeTypeAdapter implements JsonSerializer<AbstractNode>, JsonDeserializer<AbstractNode>  {
     @Override
-    public JsonElement serialize(Node node, Type type, JsonSerializationContext jsonSerializationContext) {
+    public JsonElement serialize(AbstractNode node, Type type, JsonSerializationContext jsonSerializationContext) {
         return jsonSerializationContext.serialize(node, node.getClass());
     }
 
     @Override
-    public Node deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+    public AbstractNode deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        if (!jsonElement.getAsJsonObject().has("type"))
+            return jsonDeserializationContext.deserialize(jsonElement, Node.class);
+
         return switch (NodeType.valueOf(jsonElement.getAsJsonObject().get("type").getAsString())) {
             case NOTE -> jsonDeserializationContext.deserialize(jsonElement, NoteNode.class);
             case LIST -> jsonDeserializationContext.deserialize(jsonElement, ListNode.class);
